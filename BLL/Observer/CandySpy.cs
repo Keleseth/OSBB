452cdc6d684bb78f714afd0314d5c0ce93559188
Olecsiy Gavrilyuk <Keleseth@users.noreply.github.com>
2020-06-16 13:56:45 +0300

    Add observer patern (for lab 13)

---
+++ BLL/Observer/CandySpy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BLL.Observer.Interfaces;
+
+namespace BLL.Observer
+{
+
+    public class CandySpy : IObserver
+    {
+        private IObservable _candyVan;
+
+        public CandySpy(IObservable candyVan)
+        {
+            _candyVan = candyVan;
+            _candyVan.RegisterObserver(this);
+        }
+
+        public void Update(object ob)
+        {
+            var candies = (int)ob;
+
+            Console.WriteLine($"Van sell some candies, watch out there's only {candies} left");
+        }
+    }
+}
---
+++ BLL/Observer/СandyVan.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using BLL.Observer.Interfaces;
+
+namespace BLL.Observer
+{
+    public class СandyVan : IObservable
+    {
+        List<IObserver> observers;
+        private int _candies;
+
+        public СandyVan()
+        {
+            observers = new List<IObserver>();
+            _candies = 10;
+        }
+
+        public void SellSomeCandies(int newPopulation)
+        {
+            _candies--;
+            NotifyObservers();
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var o in observers)
+            {
+                o.Update(_candies);
+            }
+        }
+    }
+}
