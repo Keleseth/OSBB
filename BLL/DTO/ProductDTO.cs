d89d87602f597125be54ce1d7e2ac7a93a6ed1ba
Olecsiy Gavrilyuk <Keleseth@users.noreply.github.com>
2020-06-16 13:50:46 +0300

    Add DTO

---
+++ BLL/DTO/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BLL.DTO
+{
+    public class ProductDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
