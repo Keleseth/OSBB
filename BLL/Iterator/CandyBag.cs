74ef3a057f103df7ce54c9da50c70b3c3c5d1234
Olecsiy Gavrilyuk <Keleseth@users.noreply.github.com>
2020-06-16 13:56:22 +0300

    Add iterator patern (for lab 12)

---
+++ BLL/Iterator/CandyBag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BLL.Factory.Model.Interfaces;
+
+namespace BLL.Iterator
+{
+    public class CandyBag : IteratorAggregate
+    {
+        List<ICandy> _collection = new List<ICandy>();
+
+        bool _direction = false;
+
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+
+        public List<ICandy> GetItems()
+        {
+            return _collection;
+        }
+
+        public void AddItem(ICandy item)
+        {
+            this._collection.Add(item);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new CandyIterator(this, _direction);
+        }
+    }
+}
