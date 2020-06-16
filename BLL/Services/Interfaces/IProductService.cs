c6aa5451a92cf41d1e8b7ae89534886c10a10e36
Olecsiy Gavrilyuk <Keleseth@users.noreply.github.com>
2020-06-16 13:51:15 +0300

    Add services

---
+++ BLL/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using BLL.DTO;
+using DAL.Entities;
+
+namespace BLL.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductDTO> GetProductAsync(Guid id);
+        Task<ProductDTO> AddProductAsync(ProductDTO region);
+        Task RemoveProductAsync(Guid id);
+    }
+}
---
+++ BLL/Services/ProductService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Services.Interfaces;
+using CCL.Security;
+using CCL.Security.Identity;
+using DAL.Entities;
+using DAL.UnitOfWork.Interfaces;
+
+namespace BLL.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(
+                              nameof(unitOfWork));
+        }
+
+        public async Task<ProductDTO> GetProductAsync(Guid id)
+        {
+            var region = await GetByIdAsync(id);
+
+            return new ProductDTO()
+            {
+                Id = region.Id,
+                Description = region.Description,
+                Name = region.Name
+            };
+        }
+
+        public async Task<ProductDTO> AddProductAsync(ProductDTO region)
+        {
+            var user = Authorization.GetUser();
+            var userRole = user.GetType();
+            if (userRole != typeof(Administrator))
+            {
+                throw new MethodAccessException();
+            }
+
+            var entity = new Product()
+            {
+                Description = region.Description,
+                Name = region.Name
+            };
+
+            var result = await _unitOfWork.InsertAsync(entity);
+            await _unitOfWork.CommitAsync();
+
+            return new ProductDTO()
+            {
+                Id = result.Id,
+                Description = result.Description,
+                Name = result.Name
+            };
+        }
+
+        public async Task RemoveProductAsync(Guid id)
+        {
+            var user = Authorization.GetUser();
+            var userRole = user.GetType();
+            if (userRole != typeof(Administrator))
+            {
+                throw new MethodAccessException();
+            }
+
+            var region = await GetByIdAsync(id);
+            _unitOfWork.Remove(region);
+            await _unitOfWork.CommitAsync();
+        }
+
+        private async Task<Product> GetByIdAsync(Guid id)
+        {
+            var region = await _unitOfWork.GetByIdAsync<Product>(id);
+
+            if (region == null)
+            {
+                throw new Exception("Product with following id was not found");
+            }
+
+            return region;
+        }
+    }
+}
